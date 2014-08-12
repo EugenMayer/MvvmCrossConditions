b51f3c67230ca4fdd335b75747252306e339dfb7
EugenMayer <eugen.mayer@digitalpatrioten.com>
2014-08-12 10:54:45 +0200

    adding refs to project, eventhough they will be broken in a standalone build for now

--- MvvmCross.Conditions.Touch.Example/ExampleSetup.cs
+++ MvvmCross.Conditions.Touch.Example/ExampleSetup.cs
@@ -3,18 +3,35 @@ using Cirrious.MvvmCross.Touch.Platform;
 using Cirrious.MvvmCross.ViewModels;
 using MvvmCross.Conditions.Core;
 using MonoTouch.UIKit;
+using Cirrious.MvvmCross.Touch.Views.Presenters;
+using Cirrious.MvvmCross.Views;
 
 namespace MvvmCross.Conditions.Touch.Example
 {
     public class ExampleSetup : MvxTouchSetup
     {
+        protected MvxApplicationDelegate _applicationDelegate;
+        protected UIWindow _window;
+
         public ExampleSetup(MvxApplicationDelegate applicationDelegate, UIWindow window) : base(applicationDelegate, window)
         {
+            _window = window;
+            _applicationDelegate = applicationDelegate;
         }
 
         protected override IMvxViewModelLoader CreateViewModelLoader()
         {
             return new PreloaderViewModelLoader();
+        }
+
+        protected override IMvxTouchViewPresenter CreatePresenter()
+        {
+            return new ConditionalTouchPresenter(_applicationDelegate, _window);
+        }
+
+        protected override IMvxViewDispatcher CreateViewDispatcher()
+        {
+            return new ConditionalTouchDispatcher(Presenter);
         }
     }
 }
